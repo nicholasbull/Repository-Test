6c83d9586758729c175c19c15c9a84b70629339e
igrainger <igrainger@IanRetinaPro.local>
2013-10-02 14:58:20 +0100

    First File
    This is my first file in my first reposiroty

---
+++ javascript.js
@@ -0,0 +1,8 @@
+<!-- hide script from old browsers
+test = new Date()
+day = test.getDate()
+month = test.getMonth()
+month = (month * 1) + 1
+year = test.getFullYear()
+document.write(" ",month,"/",day,"/",year," ")
+// end hiding script from old browsers -->
